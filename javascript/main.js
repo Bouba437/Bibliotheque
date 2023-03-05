0a9b0cc4c9948d9508c580ac7357f966bacf206d
Bouba <diarraboubaca@yahoo.fr>
2023-03-05 19:59:54 +0100

    Ajout d'un livre

--- javascript/main.js
+++ javascript/main.js
@@ -3,36 +3,65 @@ const table = document.querySelector("#tableauLivres");
 var l1 = {
     nom: "L'algorithmique selon Boubadev",
     auteur: "Boubacar Diarra",
-    page: 200
+    pages: 200
 }
 var l2 = {
     nom: "La France du 19e siècle",
     auteur: "Albert Patrick",
-    page: 500
+    pages: 500
 }
 var l3 = {
     nom: "Le monde des animaux",
     auteur: "Marc Merlin",
-    page: 250
+    pages: 250
 }
 var l4 = {
     nom: "Le virus d'Asie",
     auteur: "Milo Tya",
-    page: 120
+    pages: 120
 }
 
-var livres = [l1, l2, l3, l4];
+var biblio = [l1, l2, l3, l4];
 
-for(var i = 0; i < livres.length; i++) {
-    var livre1 = document.createElement("tr");
-    livre1.innerHTML = `<td>${livres[i].nom}</td>
-                        <td>${livres[i].auteur}</td>
-                        <td>${livres[i].page}</td>
-                        <td><button type="button" class="btn bg-warning">Modifier</button></td>
-                        <td><button type="button" class="btn bg-danger">Supprimer</button></td>`;
-    table.appendChild(livre1);
+afficherLivres();
+
+function afficherLivres() {
+    var tableauLivres = document.querySelector("#tableauLivres tbody");
+    var livres = "";
+    for(var i = 0; i < biblio.length; i++) {
+        livres += `<tr>
+                    <td>${biblio[i].nom}</td>
+                    <td>${biblio[i].auteur}</td>
+                    <td>${biblio[i].pages}</td>
+                    <td><button type="button" class="btn bg-warning">Modifier</button></td>
+                    <td><button type="button" class="btn bg-danger">Supprimer</button></td>
+                </tr>`;
+    }
+    tableauLivres.innerHTML = livres;
 }
 
 function ajoutFormulaire() {
     document.querySelector("#ajoutForm").removeAttribute("class");
+}
+
+document.querySelector("#validationAjoutForm").addEventListener("click", function(event) {
+    event.preventDefault()
+    var titre = document.querySelector("#ajoutForm #titre").value;
+    var auteur = document.querySelector("#ajoutForm #auteur").value;
+    var pages = parseInt(document.querySelector("#ajoutForm #pages").value);
+
+    ajoutLivre(titre, auteur, pages);
+    document.querySelector("#ajoutForm").reset();
+    document.querySelector("#ajoutForm").className = "d-none";
+})
+
+function ajoutLivre(titre, auteur, pages) {
+    var livre = {
+        nom: titre,
+        auteur: auteur,
+        pages: pages
+    }
+
+    biblio.push(livre);
+   afficherLivres();
 }
